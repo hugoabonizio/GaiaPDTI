e02fbb2cdf2aeed913467c760c7a4138c7ca25f6
Hugo Abonizio <hugo_abonizio@hotmail.com>
2015-12-31 20:03:04 +0000

    Portable shebang

--- app/views/documents/edit.php
+++ app/views/documents/edit.php
@@ -2,6 +2,11 @@
   <h1><?= $sections[$section][$subsection] ?></h1>
   <form action="" method="post">
     <input type="hidden" name="field" value="">
-    <textarea class="textarea" style="width: 100%; height: 40%;"><?= $content ?></textarea>
+    <div class="form-group">
+      <textarea class="textarea" style="width: 100%; height: 40%;"><?= $content ?></textarea>
+    </div>
+    <div class="form-group">
+      <button type="submit" class="btn btn-success btn-lg pull-right">Salvar</button>
+    </div>
   </form>
 </div>
