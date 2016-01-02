0f0ed91293ffded2bb259671589dc638e8a7d501
Hugo Abonizio <hugo_abonizio@hotmail.com>
2016-01-02 19:44:39 +0000

    CSS fix

--- app/views/documents/edit.php
+++ app/views/documents/edit.php
@@ -1,17 +1,15 @@
 <div class="pdti-sections" style="margin: 0 auto;">
-  <div style="padding: 15px;">
-    <h1><?= $sections[$section][$subsection] ?></h1>
-    <form action="<?= url_for('update', 'documents', $_SESSION['document_id']) ?>" method="post">
-      <input type="hidden" name="section" value="<?= $section ?>">
-      <input type="hidden" name="subsection" value="<?= $subsection ?>">
-      <div class="form-group">
-        <textarea name="content" class="textarea" style="width: 100%; height: 50%;"><?= $content ?></textarea>
+  <h1><?= $sections[$section][$subsection] ?></h1>
+  <form action="<?= url_for('update', 'documents', $_SESSION['document_id']) ?>" method="post">
+    <input type="hidden" name="section" value="<?= $section ?>">
+    <input type="hidden" name="subsection" value="<?= $subsection ?>">
+    <div class="form-group">
+      <textarea name="content" class="textarea" style="width: 100%; height: 50%;"><?= $content ?></textarea>
+    </div>
+    <div class="row">
+      <div class="col-lg-12">
+        <button type="submit" class="btn btn-success btn-lg pull-right">Salvar</button>
       </div>
-      <div class="row">
-        <div class="col-lg-12">
-          <button type="submit" class="btn btn-success btn-lg pull-right">Salvar</button>
-        </div>
-      </div>
-    </form>
-  </div>
+    </div>
+  </form>
 </div>
